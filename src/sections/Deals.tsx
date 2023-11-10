2d6936222e9cc843b33bfbdf5e9013af67ab5e52
cassandraGoose <cassandra.torske@gmail.com>
2023-11-09 19:55:41 -0700

    style deals for mobile

--- src/sections/Deals.tsx
+++ src/sections/Deals.tsx
@@ -6,7 +6,7 @@ import Pause from '../components/Pause';
 
 function Deals() {
   return (
-    <section className='h-big w-8/12 gap-3 grid grid-cols-10 grid-rows-6'>
+    <section className='h-big md:w-8/12 w-11/12 gap-3 md:grid md:grid-cols-10 md:grid-rows-6 flex flex-col'>
       <BigDeal />
       <Rewards />
       <ComboDeal />
