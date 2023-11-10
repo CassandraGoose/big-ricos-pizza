6d14435844e58c5ce271a9ffddacb2d073b150bc
cassandraGoose <cassandra.torske@gmail.com>
2023-11-09 20:05:02 -0700

    update lower menu for mobile

--- src/sections/LowerMenu.tsx
+++ src/sections/LowerMenu.tsx
@@ -1,11 +1,11 @@
 function LowerMenu() {
   return (
     <section className='flex justify-center items-center'>
-      <ul className='flex justify-around items-center w-8/12 my-16 uppercase font-medium text-blue text-lg'>
-        <li>Locations</li>
+      <ul className='flex md:flex-row flex-col justify-around items-center w-8/12 my-16 uppercase font-medium text-blue text-lg sm:leading-10'>
+        <li className="sm:hidden">Locations</li>
         <li>Email & Text Offers</li>
         <li>Gift Cards</li>
-        <li>Tracker</li>
+        <li className="sm:hidden">Tracker</li>
         <li>Nutrition</li>
       </ul>
     </section>
