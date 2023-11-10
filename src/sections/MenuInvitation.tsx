4e025158e596b1e95ddbd860aa414ff81a7df601
cassandraGoose <cassandra.torske@gmail.com>
2023-11-09 17:10:26 -0700

    make footer accordions functional

--- src/sections/MenuInvitation.tsx
+++ src/sections/MenuInvitation.tsx
@@ -1,4 +1,5 @@
 import Button from '../components/Button';
+import BookIcon from '../assets/book_icon.svg';
 
 function MenuInvitation() {
   return (
@@ -9,16 +10,7 @@ function MenuInvitation() {
       <div className='bg-menu-invitation bg-cover bg-center flex p-2 w-full py-14 mt-6'>
         <div className='flex flex-col items-center justify-center ml-8'>
           <div className='h-auto w-12 mx-14 mb-6'>
-            <svg viewBox='0 0 64.83 95.55' xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='m2 18h60.83v75.55h-60.83zm47.83-16-47.83 16h47.83zm-22.83 40.12h9.28m-15.94 9.86h22.6m-18.69 9.86h14.78m-18.69 9.86h22.6'
-                fill='none'
-                stroke='#fff'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='4'
-              />
-            </svg>
+           <img src={BookIcon} alt='book icon' />
           </div>
           <Button text='View Full Menu' bgColor='red' xPadding='5' />
         </div>
--- src/sections/SplashFooter.tsx
+++ src/sections/SplashFooter.tsx
@@ -1,13 +1,63 @@
+import { useState } from 'react';
 import placeholderLogo from '../assets/placeholder.webp';
 import placeHolderRadio from '../assets/placeholder_radio.png';
 import placeHolderSheriff from '../assets/placeholder_sheriff.png';
+import expandArrow from '../assets/expand_arrow.svg';
 
 function SplashFooter() {
+  const accordionData = [
+    {
+      title: 'Pay the Price for the Meal Deal',
+      content: `You must ask for this offer while available. If you do not ask,
+        you will be charged a fee for stewed tomatoes according to the
+        standard income sliding scale for stewed tomatoes. This offer is
+        only available at Big Rico's Pizza. There may be an extra charge
+        depending on toppings, customizations, and sauces.`,
+      isOpen: true,
+    },
+    {
+      title: 'Carryout Options',
+      content: `You must explicitly and clearly request carryout. If you do not,
+        you will be chained to the table and forced to eat your meal in
+        the restaurant. This is in accordance with City Council
+        requirements.`,
+      isOpen: false,
+    },
+    {
+      title: 'Allergen Warning',
+      content: `Wheat and Wheat By-Products are a dangerous material and are
+        banned by the City Council. We cannot serve you Wheat or Wheat
+        By-Products and you will not find any Wheat or Wheat By-Products
+        on the premises. We offer gluten free pizza, stewed tomatoes,
+        melted cheeses, and other delicious dishes in place of our
+        original pizza. Please do not ask for pizza containing Wheat or
+        Wheat By-Products as this may result in your immediate arrest
+        and subsequent quarentine.`,
+      isOpen: false,
+    },
+  ];
+
+  const [accordions, updateAccordions] = useState(accordionData);
+
+  const toggleAccordion = (title: string) => {
+    const updatedAccordions = accordions.map((accordion) => {
+      if (accordion.title === title) {
+        return {
+          ...accordion,
+          isOpen: !accordion.isOpen,
+        };
+      }
+      return accordion;
+    });
+
+    updateAccordions(updatedAccordions);
+  };
+
   return (
-    <section>
-      <div className='bg-light-grey text-dark-grey flex justify-center items center'>
+    <section className='mt-16'>
+      <div className='bg-light-grey text-dark-grey flex justify-center items center py-16'>
         <div className='w-8/12 flex flex-col items-center justify-between'>
-          <div className='w-full flex justify-between text-lg font-light leading-10 pt-12'>
+          <div className='w-full flex justify-between text-lg font-light leading-10'>
             <ul className='footer-ul' aria-label='Our Company'>
               <li>Corporate</li>
               <li>Jobs</li>
@@ -55,7 +105,37 @@ function SplashFooter() {
               alt="sheriff's secret police logo"
             />
           </div>
-          <div>legal information below.</div>
+          <div className='flex flex-col w-full'>
+            {accordions.map((accordion, i) => (
+              <div
+                key={accordion.title}
+                onClick={() => toggleAccordion(accordion.title)}>
+                <h6
+                  className='w-full my-2'
+                  key={accordion.title}
+                  id={`accordion-header-${i}`}>
+                  <button
+                    className='flex items-center justify-start font-bold'
+                    aria-contols={`accordion-panel-${i}`}>
+                    {accordion.title}
+                    <img
+                      className='h-4 w-auto ml-2'
+                      src={expandArrow}
+                      alt='arrow'
+                    />
+                  </button>
+                </h6>
+                <div
+                  className={`${
+                    accordion.isOpen ? 'max-h-16' : 'max-h-0 collapse'
+                  } transition-[max-height] duration-150 ease-in-out`}
+                  aria-expanded={accordion.isOpen}
+                  aria-labelledby={`accordion-header-${i}`}>
+                  {accordion.content}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
