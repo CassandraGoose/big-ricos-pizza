393a3854aecfb8a52e83924cbe5ae01fe247b4dd
cassandraGoose <cassandra.torske@gmail.com>
2023-12-05 12:52:42 -0700

    make modal work for items below fold

--- src/Start/LowerMenu.tsx
+++ src/Start/LowerMenu.tsx
@@ -1,12 +1,40 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import ModalContext from "../Context/ModalContext";
+import ComingSoon from "../ComingSoon";
+
 function LowerMenu() {
+  const { setModalVisible, setModalContent } = useContext(ModalContext);
+
+  const delegatedClick = (e:React.MouseEvent<HTMLLIElement>) => {
+    const target = e.target as HTMLLIElement;
+    if (target.tagName === "LI") {
+      setModalContent(<ComingSoon />);
+      setModalVisible(true);
+    }
+  };
+
   return (
-    <section className='flex justify-center items-center'>
-      <ul className='flex md:flex-row flex-col justify-around items-center w-8/12 my-16 uppercase font-medium text-blue text-lg leading-10 md:leading-none'>
-        <li className='hidden md:inline'>Locations</li>
-        <li>Email & Text Offers</li>
-        <li>Gift Cards</li>
-        <li className='hidden md:inline'>Tracker</li>
-        <li>Nutrition</li>
+    <section className="flex justify-center items-center">
+      <ul className="flex md:flex-row flex-col justify-around items-center w-8/12 my-16 uppercase font-medium text-blue text-lg leading-10 md:leading-none">
+        <Link to={`restaurants`}>
+          <li className="hidden md:inline cursor-pointer">Locations</li>
+        </Link>
+        <li className="cursor-pointer" onClick={(e) => delegatedClick(e)}>
+          Email & Text Offers
+        </li>
+        <li className="cursor-pointer" onClick={(e) => delegatedClick(e)}>
+          Gift Cards
+        </li>
+        <li
+          className="cursor-pointer hidden md:inline"
+          onClick={(e) => delegatedClick(e)}
+        >
+          Tracker
+        </li>
+        <li className="cursor-pointer" onClick={(e) => delegatedClick(e)}>
+          Nutrition
+        </li>
       </ul>
     </section>
   );
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,7 @@ function Modal() {
   const { setModalVisible, modalContent } = useContext(ModalContext);
 
   return (
-    <section className="z-50 flex justify-center items-center h-screen w-screen absolute bg-white/[0.7]">
+    <section className="z-50 fixed inset-0 flex justify-center items-center h-full w-screen bg-white/[0.7]">
       <div className="flex flex-col h-5/6 w-5/12 overflow-y-auto bg-white border border-light-grey p-2 rounded">
         <button className="self-end" onClick={() => setModalVisible(false)}>
           <img className="w-4 h-4 m-2" src={x} alt="x to close" />
