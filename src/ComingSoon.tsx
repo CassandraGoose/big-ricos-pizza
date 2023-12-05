453350939bf218406ae39632c7c42c9dddaacf05
cassandraGoose <cassandra.torske@gmail.com>
2023-12-05 12:37:35 -0700

    hook up rewards, tracker to coming soon; hook up start order to order page

---
+++ src/ComingSoon.tsx
@@ -0,0 +1,14 @@
+function ComingSoon() {
+  return (
+    <>
+      <div className="flex justify-center flex-col items-center">
+        <p className="text-3xl font-bold uppercase text-center">
+          This feature is coming soon.
+        </p>
+        <p className="text-center mt-10">I know it's weird to even allude to features without them being finished, but this isn't a serious project and I didn't think I'd build it out this far.</p>
+      </div>
+    </>
+  );
+}
+
+export default ComingSoon;
--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,31 +1,32 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import ModalContext from '../Context/ModalContext';
-import Logo from '../assets/logo.svg';
-import Cart from '../assets/cart.svg';
-import Hamburger from './Hamburger';
-import FreeRicos from './FreeRicos';
-import MobileLowerList from './MobileLowerList';
-import SignIn from '../SignIn';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import ModalContext from "../Context/ModalContext";
+import Logo from "../assets/logo.svg";
+import Cart from "../assets/cart.svg";
+import Hamburger from "./Hamburger";
+import FreeRicos from "./FreeRicos";
+import MobileLowerList from "./MobileLowerList";
+import SignIn from "../SignIn";
+import ComingSoon from "../ComingSoon";
 
 function Navigation() {
   const { setModalVisible, setModalContent } = useContext(ModalContext);
   return (
-    <nav className='bg-blue text-white flex md:flex-row flex-col justify-center items-center md:px-auto font-oswald shadow-lg shadow-dark-grey/50'>
-      <ul className='flex md:w-8/12 w-11/12 items-center justify-between uppercase md:h-20 h-14'>
-        <li className='md:hidden'>
+    <nav className="bg-blue text-white flex md:flex-row flex-col justify-center items-center md:px-auto font-oswald shadow-lg shadow-dark-grey/50">
+      <ul className="flex md:w-8/12 w-11/12 items-center justify-between uppercase md:h-20 h-14">
+        <li className="md:hidden">
           <Hamburger />
         </li>
-        <li className='cursor-pointer flex items-center justify-center'>
+        <li className="cursor-pointer flex items-center justify-center">
           <Link to={`/`}>
             <img
               src={Logo}
-              className='h-12 min-w-[40px]'
-              alt='Definitely Big Ricos logo'
+              className="h-12 min-w-[40px]"
+              alt="Definitely Big Ricos logo"
             />
           </Link>
         </li>
-        <li className='p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden'>
+        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
           <Link to={`restaurants`}>
             <span>
               Order
@@ -34,35 +35,54 @@ function Navigation() {
             </span>
           </Link>
         </li>
-        <li className='p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden'>
+        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
           <Link to={`restaurants`}>Locations</Link>
         </li>
-        <li className='p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden'>
+        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
           <Link to={`menu`}>Menu</Link>
         </li>
-        <li className='p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden'>
+        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
           <Link to={`coupons`}>Coupons</Link>
         </li>
-        <li className='p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden'>
+        <li
+          className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden"
+          onClick={() => {
+            setModalContent(<ComingSoon />);
+            setModalVisible(true);
+          }}
+        >
           Tracker
         </li>
-        <li className='p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden'>
+        <li
+          className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden"
+          onClick={() => {
+            setModalContent(<ComingSoon />);
+            setModalVisible(true);
+          }}
+        >
           Rewards
         </li>
-        <li className='md:inline hidden'>
+        <li className="md:inline hidden">
           <FreeRicos light={false} />
         </li>
         <li
-          className='text-sm md:flex flex-col items-center justify-center p-4 h-full bg-dark-blue cursor-pointer hidden'
+          className="text-sm md:flex flex-col items-center justify-center p-4 h-full bg-dark-blue cursor-pointer hidden"
           onClick={() => {
             setModalContent(<SignIn />);
             setModalVisible(true);
-          }}>
+          }}
+        >
           <span>Sign in & earn</span>
           <span>rewards</span>
         </li>
-        <li className='p-4 flex flex-col justify-center cursor-pointer'>
-          <img src={Cart} className='h-6' alt='cart icon' />
+        <li
+          className="p-4 flex flex-col justify-center cursor-pointer"
+          onClick={() => {
+            setModalContent(<ComingSoon />);
+            setModalVisible(true);
+          }}
+        >
+          <img src={Cart} className="h-6" alt="cart icon" />
           <span>cart</span>
         </li>
       </ul>
