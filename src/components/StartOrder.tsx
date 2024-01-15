34f362800e95b6554a406e7d4660a8ac54f7badc
cassandraGoose <cassandra.torske@gmail.com>
2024-01-15 16:01:56 -0700

    make menu component work on medium screens

--- src/components/StartOrder.tsx
+++ src/components/StartOrder.tsx
@@ -3,10 +3,17 @@ import FakeButton from './FakeButton';
 function StartOrder() {
   return (
     <div className='w-8/12 rounded-md p-4 flex justify-around items-center bg-blue text-white'>
-      <p className='text-3xl uppercase font-bold'>Start your order</p>
-      <FakeButton text='Delivery' xPadding='14' bgColor='red' nextPage="/restaurants" />
-      <p className='font-dm lowercase text-xl mx-4'>or</p>
-      <FakeButton text='Carryout' xPadding='14' bgColor='red' nextPage="/restaurants" />
+      <p className='xl:text-3xl lg:text-2xl md:text-xl text-lg uppercase font-bold'>Start your order</p>
+      <div className="md:visible md:flex hidden items-center">
+        <FakeButton text='Delivery' xPadding='14' bgColor='red' nextPage="/restaurants" />
+        <p className='font-dm lowercase text-xl mx-4'>or</p>
+        <FakeButton text='Carryout' xPadding='14' bgColor='red' nextPage="/restaurants" />
+      </div>
+      <div className="visible md:hidden flex items-center ">
+        <FakeButton text='Delivery' xPadding='5' bgColor='red' nextPage="/restaurants" />
+        <p className='font-dm lowercase text-xl mx-4'>or</p>
+        <FakeButton text='Carryout' xPadding='5' bgColor='red' nextPage="/restaurants" />
+      </div>
     </div>
   );
 }
