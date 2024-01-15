cd38df417d977d6955bb21eed6be2df0f1736903
cassandraGoose <cassandra.torske@gmail.com>
2024-01-15 12:30:51 -0700

    fix lower menu list issue

--- src/Start/LowerMenu.tsx
+++ src/Start/LowerMenu.tsx
@@ -6,7 +6,9 @@ import ComingSoon from "../ComingSoon";
 function LowerMenu() {
   const { setModalVisible, setModalContent } = useContext(ModalContext);
 
-  const delegatedClick = (e:React.MouseEvent<HTMLLIElement> | React.MouseEvent<HTMLUListElement>) => {
+  const delegatedClick = (
+    e: React.MouseEvent<HTMLLIElement> | React.MouseEvent<HTMLUListElement>
+  ) => {
     const target = e.target as HTMLLIElement;
     if (target.tagName === "LI") {
       setModalContent(<ComingSoon />);
@@ -16,25 +18,17 @@ function LowerMenu() {
 
   return (
     <section className="flex justify-center items-center">
-      <ul className="flex md:flex-row flex-col justify-around items-center w-8/12 my-16 uppercase font-medium text-blue text-lg leading-10 md:leading-none" onClick={(e) => delegatedClick(e)}>
-        <Link to={`restaurants`}>
-          <li className="hidden md:inline cursor-pointer">Locations</li>
-        </Link>
-        <li className="cursor-pointer">
-          Email & Text Offers
+      <ul
+        className="flex md:flex-row flex-col justify-around items-center w-8/12 my-16 uppercase font-medium text-blue text-lg leading-10 md:leading-none"
+        onClick={(e) => delegatedClick(e)}
+      >
+        <li className="hidden md:inline cursor-pointer">
+          <Link to={`restaurants`}>Locations</Link>
         </li>
-        <li className="cursor-pointer">
-          Gift Cards
-        </li>
-        <li
-          className="cursor-pointer hidden md:inline"
-        
-        >
-          Tracker
-        </li>
-        <li className="cursor-pointer">
-          Nutrition
-        </li>
+        <li className="cursor-pointer">Email & Text Offers</li>
+        <li className="cursor-pointer">Gift Cards</li>
+        <li className="cursor-pointer hidden md:inline">Tracker</li>
+        <li className="cursor-pointer">Nutrition</li>
       </ul>
     </section>
   );
