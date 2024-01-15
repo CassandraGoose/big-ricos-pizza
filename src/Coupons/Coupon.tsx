555254a34b67c5ab5901d6be7be7eda98fe03a53
cassandraGoose <cassandra.torske@gmail.com>
2024-01-15 16:50:31 -0700

    adjust coupons for mobile and tablet view"

--- src/Coupons/Coupon.tsx
+++ src/Coupons/Coupon.tsx
@@ -2,29 +2,32 @@ import FakeButton from '../components/FakeButton';
 
 function Coupon({ specialInfo, dealTitle, cost }: { specialInfo: JSX.Element, dealTitle: string,  cost: JSX.Element }) {
   return (
-    <div className="grid grid-row-6 grid-cols-[30%_35%_35%] bg-deals bg-fit bg-cover bg-right-2 text-white">
-      <div className="row-start-1 col-start-1 col-end-1 row-span-5 uppercase flex flex-col justify-center items-center p-2">
+    <div className="grid grid-row-4 md:grid-cols-[30%_35%_35%] sm:grid-cols-[25%_30%_45%] bg-carryout-deal bg-[center_bottom_32rem] text-white">
+      <div className="row-start-1 col-start-1 col-span-2 row-span-3 uppercase flex flex-col justify-start items-start p-2">
         <div className="flex">
           <span className="block w-0 h-0 border-solid border-t-[1rem] border-b-[1rem] border-r-[0.5rem] border-l-0 border-t-transparent border-b-transparent border-l-transparent border-r-white"></span>
-          <p className="flex justify-center items-center bg-white text-blue px-4 text-md font-medium">
+          <p className="flex justify-center items-center bg-white text-blue px-4 md:text-md text-sm font-medium">
             {dealTitle}
           </p>
           <span className="block w-0 h-0 border-solid border-t-[1rem] border-b-[1rem] border-l-[0.5rem] border-r-0 border-t-transparent border-b-transparent border-r-transparent border-l-white"></span>
         </div>
+        <div className="h-28">
         {specialInfo}
-      </div>
-      <div className="col-start-3 row-start-2 flex flex-col p-2">
-        {cost}
-        <div className="w-full flex justify-center items-center">
-          <FakeButton text="Order Now" xPadding="8" bgColor="red" nextPage="/restaurants" />
         </div>
       </div>
-      <p className="col-start-1 col-end-4 row-start-6 text-xxs normal-case drop-shadow p-2">
+      <p className="col-start-1 col-end-3 row-start-4 md:text-xxs text-xxxs normal-case drop-shadow p-2">
         This deal stricly adheres to the City Council's permanent ban on Wheat
         and Wheat By-Products. We do not serve Wheat and Wheat By-Products.
         There are no Wheat and Wheat By-Products on the premises of Big Rico's
         Pizza
       </p>
+      <div className="col-start-3 row-start-2 row-end-5 flex flex-col p-2">
+        {cost}
+      </div>
+      <div className="col-start-3 row-start-4 w-full flex justify-center items-center">
+        <FakeButton text="Order Now" xPadding="8" bgColor="red" nextPage="/restaurants" />
+      </div>
+      
     </div>
   );
 }
--- src/Coupons/Coupons.tsx
+++ src/Coupons/Coupons.tsx
@@ -7,16 +7,17 @@ function Coupons() {
       dealTitle: "Big Deal",
       specialInfo: () => (
         <>
-          <p className="text-2xl font-medium text-shadow text-center">
+          <p className="xl:text-2xl lg:text-xl text-md font-medium text-shadow text-center">
             Pay the normal price
           </p>
+          <div className="flex flex-col justify-center items-center">
           <p className="text-md tracking-widester text-shadow">including</p>
           <div className="flex drop-shadow-lg">
             <div>
               <span className="block w-0 h-0 border-solid border-t-0 border-r-[1rem] border-b-[1rem] border-l-0 border-t-transparent border-r-red border-b-transparent border-l-transparent"></span>
               <span className="block w-0 h-0 border-solid border-t-0 border-b-[1rem] border-l-[1rem] border-r-0 border-r-transparent border-t-transparent border-l-transparent border-b-red"></span>
             </div>
-            <p className="flex justify-center items-center bg-red px-4 text-lg">
+            <p className="flex justify-center items-center bg-red px-4 xl:text-lg text-md">
               <span className="text-shadow italic">New</span>
               &nbsp;&nbsp;Stewed
             </p>
@@ -25,39 +26,40 @@ function Coupons() {
               <span className="block w-0 h-0 border-solid border-t-0 border-b-[1rem] border-r-[1rem] border-l-0 border-l-transparent border-t-transparent border-r-transparent border-b-red"></span>
             </div>
           </div>
-          <p className="text-lg tracking-widester">tomatoes</p>
+          <p className="xl:text-lg xl:tracking-widester text-md tracking-wider">tomatoes</p>
+          </div>
         </>
       ),
       cost: () => (
-        <div className="grid grid-col-5 grid-row-2 w-1/2">
-          <span className="custom-outline text-white text-6xl col-start-1 row-start-1 font-bold">
+        <div className="grid grid-col-5 grid-row-2 w-1/2 xl:text-6xl md:text-4xl">
+          <span className="custom-outline text-white col-start-1 row-start-1 font-bold">
             $
           </span>
           <span
-            className="custom-outline-shadow text-white text-6xl col-start-1 row-start-1 font-bold"
+            className="custom-outline-shadow text-white col-start-1 row-start-1 font-bold"
             aria-hidden="true"
           >
             $
           </span>
-          <span className="custom-outline text-white text-9xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]">
+          <span className="custom-outline text-white xl:text-9xl text-8xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]">
             9
           </span>
           <span
-            className="custom-outline-shadow text-white text-9xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]"
+            className="custom-outline-shadow text-white xl:text-9xl text-8xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]"
             aria-hidden="true"
           >
             9
           </span>
-          <span className="custom-outline text-white text-6xl col-start-3 col-end-5 row-start-1 font-bold">
+          <span className="custom-outline text-white col-start-3 col-end-5 row-start-1 font-bold">
             99
           </span>
           <span
-            className="custom-outline-shadow text-white text-6xl col-start-3 col-end-5 row-start-1 font-bold"
+            className="custom-outline-shadow text-white col-start-3 col-end-5 row-start-1 font-bold"
             aria-hidden="true"
           >
             99
           </span>
-          <p className="col-start-3 col-end-4 row-start-2 text-white text-shadow normal-case italic text-3xl tracking-wider font-crimson">
+          <p className="col-start-3 col-end-4 row-start-2 text-white text-shadow normal-case italic xl:text-3xl lg:text-2xl text-xl tracking-wider font-crimson">
             each
           </p>
         </div>
@@ -66,40 +68,40 @@ function Coupons() {
     {
       dealTitle: "Combo Deal",
       specialInfo: () => (
-        <p className="text-2xl font-medium text-shadow text-center">
+        <p className="xl:text-2xl lg:text-xl md:text-lg text-sm font-medium text-shadow text-center">
           Buy one handful of stewed tomatoes, get one free!
         </p>
       ),
       cost: () => (
-        <div className="grid grid-col-5 grid-row-2 w-1/2">
-          <span className="custom-outline text-white text-6xl col-start-1 row-start-1 font-bold">
+        <div className="grid grid-col-5 grid-row-2 w-1/2 xl:text-6xl md:text-4xl">
+          <span className="custom-outline text-white col-start-1 row-start-1 font-bold">
             $
           </span>
           <span
-            className="custom-outline-shadow text-white text-6xl col-start-1 row-start-1 font-bold"
+            className="custom-outline-shadow text-white col-start-1 row-start-1 font-bold"
             aria-hidden="true"
           >
             $
           </span>
-          <span className="custom-outline text-white text-9xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]">
+          <span className="custom-outline text-white xl:text-9xl text-8xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]">
             5
           </span>
           <span
-            className="custom-outline-shadow text-white text-9xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]"
+            className="custom-outline-shadow text-white xl:text-9xl text-8xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]"
             aria-hidden="true"
           >
             5
           </span>
-          <span className="custom-outline text-white text-6xl col-start-3 col-end-5 row-start-1 font-bold">
+          <span className="custom-outline text-white col-start-3 col-end-5 row-start-1 font-bold">
             99
           </span>
           <span
-            className="custom-outline-shadow text-white text-6xl col-start-3 col-end-5 row-start-1 font-bold"
+            className="custom-outline-shadow text-white col-start-3 col-end-5 row-start-1 font-bold"
             aria-hidden="true"
           >
             99
           </span>
-          <p className="col-start-3 col-end-4 row-start-2 text-white text-shadow normal-case italic text-3xl tracking-wider font-crimson">
+          <p className="col-start-3 col-end-4 row-start-2 text-white text-shadow normal-case italic xl:text-3xl lg:text-2xl text-xl tracking-wider font-crimson">
             each
           </p>
         </div>
@@ -108,52 +110,53 @@ function Coupons() {
     {
       dealTitle: "9.99 Deal",
       specialInfo: () => (
-        <p className="text-lg text-white text-shadow font-medium">
+        <p className="xl:text-2xl lg:text-xl md:text-lg text-sm  text-white text-shadow font-medium">
           All 1 Topping Gluten-Free Pizzas or stewed tomatoes or bowls of cheese
         </p>
       ),
       cost: () => (
-        <div className="grid grid-col-5 grid-row-2 w-1/2">
-          <span className="custom-outline text-white text-6xl col-start-1 row-start-1 font-bold">
+        <div className="grid grid-col-5 grid-row-2 w-1/2 xl:text-6xl text-4xl">
+          <span className="custom-outline text-white col-start-1 row-start-1 font-bold">
             $
           </span>
           <span
-            className="custom-outline-shadow text-white text-6xl col-start-1 row-start-1 font-bold"
+            className="custom-outline-shadow text-white col-start-1 row-start-1 font-bold"
             aria-hidden="true"
           >
             $
           </span>
-          <span className="custom-outline text-white text-9xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]">
+          <span className="custom-outline text-white xl:text-9xl text-8xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]">
             9
           </span>
           <span
-            className="custom-outline-shadow text-white text-9xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]"
+            className="custom-outline-shadow text-white xl:text-9xl text-8xl col-start-2 col-end-3 row-start-1 row-end-3 font-bold mt-[-1rem]"
             aria-hidden="true"
           >
             9
           </span>
-          <span className="custom-outline text-white text-6xl col-start-3 col-end-5 row-start-1 font-bold">
+          <span className="custom-outline text-white col-start-3 col-end-5 row-start-1 font-bold">
             99
           </span>
           <span
-            className="custom-outline-shadow text-white text-6xl col-start-3 col-end-5 row-start-1 font-bold"
+            className="custom-outline-shadow text-white col-start-3 col-end-5 row-start-1 font-bold"
             aria-hidden="true"
           >
             99
           </span>
-          <p className="col-start-3 col-end-4 row-start-2 text-white text-shadow normal-case italic text-3xl tracking-wider font-crimson">
+          <p className="col-start-3 col-end-4 row-start-2 text-white text-shadow normal-case italic xl:text-3xl lg:text-2xl text-xl tracking-wider font-crimson">
             each
           </p>
         </div>
       ),
     },
   ];
+
   return (
-    <section className="flex flex-col justify-center items-center mt-2">
-      <div className="w-3/4 self-end">
+    <section className="w-full flex flex-col justify-center items-center mt-2">
+      <div className="md:w-3/4 w-full self-center flex justify-center items-center">
         <StartOrder />
       </div>
-      <div className="w-1/2 flex flex-col mt-4">
+      <div className="md:w-1/2 w-11/12 flex flex-col mt-4">
         <h1 className="text-4xl font-bold text-dark-grey uppercase self-start">
           Big Rico's Coupons
         </h1>
