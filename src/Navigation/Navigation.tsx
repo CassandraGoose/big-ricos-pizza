e20506ffa30dbfb042b7c18386c9fd2853a05852
cassandraGoose <cassandra.torske@gmail.com>
2024-01-15 13:07:08 -0700

    fix highlight active link

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ModalContext from "../Context/ModalContext";
 import Logo from "../assets/icons/logo.webp";
 import Cart from "../assets/icons/cart.svg";
@@ -18,31 +18,31 @@ function Navigation() {
           <Hamburger />
         </li>
         <li className="cursor-pointer flex items-center justify-center">
-          <Link to={`/`}>
+          <NavLink to={`/`}>
             <img
               src={Logo}
               className="h-12 min-w-[40px]"
               alt="Definitely Big Ricos logo"
             />
-          </Link>
+          </NavLink>
         </li>
-        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
-          <Link to={`restaurants`}>
-            <span>
+        <li className="md:flex items-center hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
+          <NavLink to={`restaurants`}>
+            <span className="h-full w-full p-4 flex items-center">
               Order
               <br />
               Online
             </span>
-          </Link>
+          </NavLink>
         </li>
         <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
-          <Link to={`restaurants`}>Locations</Link>
+          <NavLink to={`restaurants`}>Locations</NavLink>
         </li>
-        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
-          <Link to={`menu`}>Menu</Link>
+        <li className="md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
+          <NavLink to={`menu`} className={(({ isActive }) => (isActive ? 'bg-dark-grey p-4 h-full flex items-center': 'p-4'))}>Menu</NavLink>
         </li>
-        <li className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
-          <Link to={`coupons`}>Coupons</Link>
+        <li className="md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden">
+          <NavLink to={`coupons`} className={(({ isActive }) => (isActive ? 'bg-dark-grey p-4 h-full flex items-center': 'p-4'))}>Coupons</NavLink>
         </li>
         <li
           className="p-4 md:flex items-center h-full hover:shadow-menu transition-shadow duration-300 ease-linear delay-0 cursor-pointer md:visible hidden"
